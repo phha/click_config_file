e3a825624a210d4e3c281560e3caaf26ee5fb231
Philipp Hack <philipp.hack@gmail.com>
2018-01-22 13:25:21 +0100

    Improved setup info

--- setup.py
+++ setup.py
@@ -1,9 +1,20 @@
 from setuptools import setup
 
-
 setup(
-        name='click_config_file',
-        version='0.2',
-        py_modules=['click_config_file'],
-        install_requires=['click >= 6.7', 'configobj >= 5.0.6'],
-        )
+    name='click_config_file',
+    author='Philipp Hack',
+    author_email='philipp.hack@gmail.com',
+    url='http://github.com/phha/click_config_file',
+    version='0.4.0',
+    license='MIT',
+    description='Configuration file support for click applications.',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+    ],
+    py_modules=['click_config_file'],
+    install_requires=['click >= 6.7', 'configobj >= 5.0.6'],
+)
