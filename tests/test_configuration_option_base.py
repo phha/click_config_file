ee555b11fbe15e1f1fd2084b6acedb4d83e69514
Leopold Talirz <leopold.talirz@gmail.com>
2019-04-24 10:22:04 +0200

    refactor tests & configuration_option_base
     * click_config_file: move callback definition to top level
       in order to clarify entanglement of variables
    
     * click_config_file: decision on whether to call config provider
       needs to occur inside callback (since specifying a `value` also
       needs to trigger it)
    
     * move tests from configuration_option to configuration_option_base
       (except for those that should work only with configuration_option)

--- tests/test_configuration_option_base.py
+++ tests/test_configuration_option_base.py
@@ -57,7 +57,8 @@ def test_config_precedence_no_config(runner):
     assert result.output == 'Hello World\n'
 
 
-def test_config_value_replacement(runner, cfgfile):
+def test_config_value_no_replacement(runner, cfgfile):
+    """Test that config does not replace variable of other name."""
     @click.command()
     @click.option('--whom', default='World', envvar='CLICK_TEST_WHO')
     @configuration_option_base()
@@ -71,6 +72,42 @@ def test_config_value_replacement(runner, cfgfile):
     ))
     assert not result.exception
     assert result.output == 'Hello World\n'
+
+def test_broken_config(runner, tmpdir):
+    @click.command()
+    @configuration_option_base()
+    def cli():
+        pytest.fail("Callback should not be called if config is broken")
+
+    cfgfile = tmpdir.join('config')
+    cfgfile.write("Ceci n'est pas une config.")
+
+    result = runner.invoke(cli, (
+        '--config',
+        str(cfgfile),
+    ))
+    assert re.search(r'Error reading configuration file',
+                     result.output) is not None
+    assert result.exception
+    assert result.exit_code != 0
+
+
+
+def test_config_precedence_override_default(runner, cfgfile):
+    @click.command()
+    @click.option('--who', default='World', envvar='CLICK_TEST_WHO')
+    @configuration_option_base()
+    def cli(who):
+        assert who == 'Universe'
+        click.echo("Hello {}".format(who))
+
+    result = runner.invoke(
+        cli, (
+            '--config',
+            str(cfgfile),
+        ), env={})
+    assert not result.exception
+    assert result.output == 'Hello Universe\n'
 
 
 def test_config_precedence_cli_option(runner, cfgfile):
@@ -230,4 +267,73 @@ def test_custom_callback_when_unset(runner):
     ))
     assert not result.exception
     assert result.output == 'bar\n'
+    assert result.exit_code == 0
+
+def test_custom_provider(runner, cfgfile):
+    def mock_provider(path, name):
+        assert path == str(cfgfile.realpath())
+        assert name == 'cli'
+        return {'whom': 'World'}
+
+    @click.command()
+    @click.option('--whom')
+    @configuration_option_base(provider=mock_provider)
+    def cli(whom):
+        assert whom == 'World'
+        click.echo("Hello {}".format(whom))
+
+    result = runner.invoke(cli, (
+        '--config',
+        str(cfgfile),
+    ))
+    assert not result.exception
+    assert result.output == 'Hello World\n'
+
+
+def test_argument_basic(runner, cfgfile):
+    cfgfile.write('arg = "foo"')
+
+    @click.command()
+    @click.argument('arg')
+    @configuration_option_base()
+    def cli(arg):
+        assert arg == 'foo'
+        click.echo(arg)
+
+    result = runner.invoke(cli, ['--config', str(cfgfile)])
+
+    assert not result.exception
+    assert result.exit_code == 0
+    assert result.output == ''.join(('foo', '\n',))
+
+
+def test_argument_variadic(runner, cfgfile):
+    cfgfile.write('arg = ["foo", "bar"]')
+
+    @click.command()
+    @click.argument('arg', nargs=2)
+    @configuration_option_base()
+    def cli(arg):
+        assert arg == ('foo', 'bar')
+        for a in arg:
+            click.echo(a)
+
+    result = runner.invoke(cli, ['--config', str(cfgfile)])
+
+    assert not result.exception
+    assert result.exit_code == 0
+    assert result.output == ''.join(('foo', '\n', 'bar', '\n',))
+
+
+def test_argument_file(runner, cfgfile):
+    @click.command()
+    @click.argument('arg', type=click.File())
+    @configuration_option_base()
+    def cli(arg):
+        assert arg.read() == cfg
+
+    cfg = 'arg = "{}"'.format(cfgfile)
+    cfgfile.write(cfg)
+    result = runner.invoke(cli, ['--config', str(cfgfile)])
+    assert not result.exception
     assert result.exit_code == 0
